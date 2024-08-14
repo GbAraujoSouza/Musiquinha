3e5b8a00d66c0c8c03ea40ad13936266008399ab
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2024-08-14 08:30:09 -0300

    config: default theme file

---
+++ frontend/src/theme/index.ts
@@ -0,0 +1,12 @@
+export default {
+  COLORS: {
+    BASE: "#1E1E2E",
+    SURFACE0: "#313244",
+    SURFACE1: "#45475A",
+    MAUVE: "#CBA6F7",
+    RED: "#F38BA8",
+    PEACH: "#FAB387",
+    BLUE: "#B4BEFE",
+    TEXT: "#CDD6F4",
+  }
+}
