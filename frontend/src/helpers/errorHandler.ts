14dd3da37ad36306391980e9adf19d09564333ca
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2024-08-18 21:05:22 -0300

    feat: axios error handler

---
+++ frontend/src/helpers/errorHandler.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useNavigation } from "@react-navigation/native";
+
+const navigation = useNavigation();
+
+export const handleError = (error: any) => {
+  if (axios.isAxiosError(error)) {
+    const err = error.response;
+    if (Array.isArray(err?.data.errors)) {
+      for (let val of err.data.errors) {
+        toast.warning(val.description);
+      }
+    } else if (typeof err?.data.errors === "object") {
+      for (let e in err?.data.errors) {
+        toast.warning(err.data.errors[e][0]);
+      }
+    } else if (err?.data) {
+      toast.warning(err.data);
+    } else if (err?.status == 401) {
+      toast.warning("Please login to continue");
+      navigation.navigate("login" as never);
+    } else {
+      toast.warning(err?.data);
+    }
+  }
+};
